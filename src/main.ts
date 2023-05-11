c247b8f43592f890adb3e091fe7cdfb551e3c483
Faizan Ahmed <imfaizanahmed6@gmail.com>
2023-05-12 01:28:02 +0500

    cors issue resolved

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   console.log('app is running at port 2000');
   await app.listen(2000);
 }
